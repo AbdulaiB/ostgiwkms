7aba858a3e71498a3385ff7aeb0c24702b2de406
Abdulai Bah <BAHA01@ETSU.EDU>
2023-12-03 20:00:59 -0500

    trying to pin the settings page to the generatestream page

---
+++ ostgiwkms/Services/SettingsService.cs
@@ -0,0 +1,9 @@
+﻿namespace ostgiwkms.Services
+{
+    public class SettingsService
+    {
+        public double TemperatureValue { get; set; } = 0.25;
+        public int MinLengthValue { get; set; } = 10;
+        public int MaxLengthValue { get; set;} = 100;
+    }
+}
