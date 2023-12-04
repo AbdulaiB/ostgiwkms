184297e20189c6942f99802a5c6684737c4dfc6b
Abdulai Bah <BAHA01@ETSU.EDU>
2023-12-03 21:34:53 -0500

    number change

--- ostgiwkms/Services/SettingsService.cs
+++ ostgiwkms/Services/SettingsService.cs
@@ -3,7 +3,7 @@
     public class SettingsService
     {
         public double TemperatureValue { get; set; } = 0.25;
-        public int MinLengthValue { get; set; } = 10;
-        public int MaxLengthValue { get; set;} = 100;
+        public int MinLengthValue { get; set; } = 100;
+        public int MaxLengthValue { get; set;} = 2048;
     }
 }
